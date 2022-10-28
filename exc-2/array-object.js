50230f7f3813a5bc702acc0eb77d7a2f58f0d63a
Felipe Rodriguez Letelier <virusfeli@gmail.com>
2022-10-28 17:59:20 -0300

    2nd exc

---
+++ exc-2/array-object.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Felipe",
+     25,
+      true,
+       new Date(10, 15, 1997),
+       ]
+
+const libroFavorito = {
+        title:"1984",
+     author:"George orwell",
+     fecha:"1985",
+     link:"https://www.amazon.com/-/es/George-Orwell/dp/8499890946"
+}
