71500280fed3a5c27b1962585c60d5f94889c5ee
Felipe <103608551+feli971@users.noreply.github.com>
2022-10-30 05:22:55 -0300

    Add files via upload

---
+++ String-methods.js
@@ -0,0 +1,16 @@
+const miNombre = "Raul" ;
+const miApellido = "Gimenez";
+const estudiante = `${miNombre + " " + miApellido}`;
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const howMany = estudiante.length;
+const firstLetter = miNombre.charAt([0]);
+const lastLetter = miApellido.charAt([6]);
+const spaceSlicer = estudiante.replaceAll(" ", "");
+const verifier = estudiante.includes("Raul")
+
+
+
+
+
+// ---->   777  <---- //
