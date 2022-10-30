6f39c1da519a3a7575201a19f1c5abc349c3db1a
Felipe <103608551+feli971@users.noreply.github.com>
2022-10-30 01:59:53 -0300

    Add files via upload

---
+++ factorial-break.js
@@ -0,0 +1,20 @@
+let i = 10;
+let count2 = 10;
+
+while(count2){
+    
+    if (i<=10){
+       i--
+    }
+
+    else if (count2 === 3628800){
+        break;
+    }
+        
+    
+
+    count2 = i*count2
+    
+   console.log(count2);
+
+}
---
+++ factorial-while.js
@@ -0,0 +1,9 @@
+// 10 *9 *8 *7 * 6 *5 *4 *3 *2 *1 
+let i = 9 ;
+let o = 10
+
+while ( i, o ) {
+    console.log(i, o)
+    o = o-- * i--
+} 
+
