5b7b4159da59bfd8b11ef95a130790d7a89d9968
Felipe <103608551+feli971@users.noreply.github.com>
2022-10-30 19:50:48 -0300

    Add files via upload

---
+++ Numbers.js
@@ -0,0 +1,13 @@
+let miAltura = 173;
+let miAlturaFloat = 1.73 ; 
+let miPesoFloat = 72.5;
+let miAlturaRound = Math.floor(miAlturaFloat);
+let miPesoRound = Math.floor(miPesoFloat);
+
+
+//////////////////////7 7 7 /////////////////////////////////
+
+let minValue  = Number.MIN_VALUE;
+let maxValue  = Number.MAX_VALUE;
+console.log(minValue === maxValue)
+
