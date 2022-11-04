4c39c1955b3e16769a02f4d0fb4746cd3133e8a8
Felipe <103608551+feli971@users.noreply.github.com>
2022-11-04 13:38:06 -0300

    sets

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+
+const familia = new Set( ["Eduardo","Felipe","J pierre","Catalina", "Sylvia"]);
+const feldo = "Felipe"
+const lang = "JavaScript"
+familia.add(feldo)
+familia.add(lang)
+console.log(familia)
+
