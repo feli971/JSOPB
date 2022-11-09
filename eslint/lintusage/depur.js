99d08de7c8e4772666841fe4c42f07af2310805a
Felipe <103608551+feli971@users.noreply.github.com>
2022-11-09 12:00:38 -0300

    eslint

---
+++ eslint/lintusage/depur.js
@@ -0,0 +1,30 @@
+let str_sng = 'Hola soy un texto con comillas simples';
+let str_dbl = "Hola soy un texto con comillas dobles";
+
+console.log(str_sng);
+console.log(str_dbl);
+
+let str_comillas = "El otro día me dijo literalmente \"ve a sacar la basura\""
+let str_comillas_simples = 'El otro día me dijo literalmente "ve a sacar la basura"'
+let str_comillas_dobles = "El otro día me dijo literalmente 've a sacar la basura'"
+let str_comillas_simples_2 = 'El otro día me dijo literalmente \'ve a sacar la basura\''
+
+console.log(str_comillas)
+console.log(str_comillas_simples)
+console.log(str_comillas_dobles)
+
+
+let str_backticks = `Hola esto es un string con backticks`
+
+console.log(str_backticks)
+
+let nombre = "Iñigo"
+let saludo = `Hola, ${nombre} bienvenido`
+
+console.log(saludo)
+
+
+`;
+
+console.log(plantilla)
+
